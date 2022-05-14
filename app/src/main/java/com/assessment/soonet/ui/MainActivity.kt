66bd45af26299fc6799d91f27090881106bd9b79
Prabin Bhusal <pbhusal07@gmail.com>
2022-05-14 19:08:11 +0545

    final

--- app/src/main/java/com/assessment/soonet/ui/MainActivity.kt
+++ app/src/main/java/com/assessment/soonet/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.assessment.soonet
+package com.assessment.soonet.ui
 
 import android.os.Bundle
 import android.view.View
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.assessment.soonet.R
 import com.assessment.soonet.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
